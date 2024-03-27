02f0115660fe4b70cc9acb4292079bc8bc81ed39
Jordan Thompson <Jordan.t1620@gmail.com>
2024-03-27 13:36:59 -0500

    fixed prob3 with api key

--- prob3.rb
+++ prob3.rb
@@ -1,19 +1,22 @@
 require 'httparty'
 
 def fetch_events(city)
-    api_key = '' # couldn't get a free API key
-    url = "https://www.eventbriteapi.com/v3/events/search/?q=#{city}&token=#{api_key}"
+    api_key = 'tDwWZt7bqks8301EIhNm26PY3P7phAuy'
+    url = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=#{city}&apikey=#{api_key}"
     response = HTTParty.get(url)
-  
-    if response.success?
-        events = response.parsed_response['events']
+    
+    if response.success? && response.parsed_response['_embedded']
+        events = response.parsed_response['_embedded']['events']
         puts "Upcoming events in #{city}:"
         events.each_with_index do |event, index|
-            puts "#{index + 1}. #{event['name']['text']} - Venue: #{event['venue']['name']} - Date: #{event['start']['local']}"
+            event_name = event['name']
+            event_venue = event['_embedded']['venues'][0]['name'] rescue 'Unknown venue' # Using 'rescue' to handle missing data
+            event_date = event['dates']['start']['localDate'] rescue 'Unknown date' 
+            puts "#{index + 1}. #{event_name} - Venue: #{event_venue} - Date: #{event_date}"
         end
     else
         puts "Failed to fetch events for #{city}."
     end
 end
 
-fetch_events('San Francisco')
+fetch_events('Memphis')
